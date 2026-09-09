86f76667360b5f08458ef9e798f48486e8aa26ca
skmeetaali <mitalikamble5@gmail.com>
2026-09-09 20:09:10 +0530

    allowed hosts changed again

--- hisab_project/settings.py
+++ hisab_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://hisab-qynm.onrender.com/"]
+ALLOWED_HOSTS = ["hisab-qynm.onrender.com/"]
 
 # Application definition
 
