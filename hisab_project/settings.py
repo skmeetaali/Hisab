080509c06689e87c476ff9fb7f1712b1d66d50fc
skmeetaali <mitalikamble5@gmail.com>
2026-09-13 18:57:37 +0530

    changed allowed hosts

--- hisab_project/settings.py
+++ hisab_project/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+#ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["hisab-qynm.onrender.com"]
 # Application definition
 
 INSTALLED_APPS = [
